57e7635fb27a0e3f529a5f4233847624ea7f385e
jeongkyojun <kyojun2002@naver.com>
2024-02-28 18:58:17 +0900

    MyInfo 페이지 axios 분리

---
+++ src/components/pages/MyPage/data.ts
@@ -0,0 +1,13 @@
+export const USER_INFO = {
+    name: "김싸피",
+    studentNumber: "20240228",
+    postsResponses: [
+        {
+            postId: 0,
+            title: "안녕하세요",
+            likeCount: 6,
+            commentCount: 12,
+            createdDate: "2022.02.28",
+        }
+    ],
+}
--- src/components/pages/MyPage/index.tsx
+++ src/components/pages/MyPage/index.tsx
@@ -1,39 +1,43 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
-import MyInfo from '../../organisms/MyInfo';
-import Navbar from '../../molecules/Navbar';
-import MyPosts from '../../organisms/MyPosts';
-import { StyledMyPage } from './styles';
-import getUserInfo from './getUserInfo';
-import { UserInfoTypes } from './types';
-import requestLogout from './requestLogout';
-import navigator from '../../../utils/navigator';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import MyInfo from "../../organisms/MyInfo";
+import Navbar from "../../molecules/Navbar";
+import MyPosts from "../../organisms/MyPosts";
+import { StyledMyPage } from "./styles";
+import getUserInfo from "./getUserInfo";
+import { UserInfoTypes } from "./types";
+import requestLogout from "./requestLogout";
+import navigator from "../../../utils/navigator";
+import { USER_INFO } from "./data";
 
 const MyPage = () => {
   const navigate = useNavigate();
 
-  const { data: userInfo } = useQuery<UserInfoTypes>(['userInfo'], () =>
-    getUserInfo(),
-  );
+  // const { data: userInfo } = useQuery<UserInfoTypes>(['userInfo'], () =>
+  //   getUserInfo(),
+  // );
+
+  const [userInfo] = useState(USER_INFO);
 
   const clickLogoutHandler = async () => {
-    if (!window.confirm('로그아웃 하시겠습니까?')) {
+    if (!window.confirm("로그아웃 하시겠습니까?")) {
       return;
     }
 
-    if (await requestLogout()) {
-      localStorage.removeItem('isLogin');
-      navigate('/login');
-    }
+    window.alert("axios 연결이 되지 않아 로그아웃이 불가능합니다.");
+    // if (await requestLogout()) {
+    //   localStorage.removeItem("isLogin");
+    //   navigate("/login");
+    // }
   };
 
   return (
     <StyledMyPage>
       <Navbar clickLogoHandler={navigator(navigate).main} />
       <MyInfo
-        name={userInfo ? userInfo.name : ''}
-        studentNumber={userInfo ? userInfo.studentNumber : ''}
+        name={userInfo ? userInfo.name : ""}
+        studentNumber={userInfo ? userInfo.studentNumber : ""}
         clickEditBtnHandler={navigator(navigate).myInfoEdit}
         clickLogoutHandler={clickLogoutHandler}
       />
